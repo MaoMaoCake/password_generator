e51c491a659beffed226407a2cd243432d4c60ac
MaoMaoCake <55589246+MaoMaoCake@users.noreply.github.com>
2020-01-13 16:59:26 +0700

    optimized codes

--- generator.py
+++ generator.py
@@ -86,11 +86,11 @@ class Ui_MainWindow(object):
 
     def generatepw(self):
         from random import sample, randint
-        lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                 "u", "v", "w", "x", "y", "z"]
-        upper = [i.upper() for i in lower]
-        special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "{", "}", "[", "]", "|", ";", ":",
-                   "'", '"', ",", "<", ".", ">", "/", "?", "`", "~"]
+        lower = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                 "u", "v", "w", "x", "y", "z"}
+        upper = {i.upper() for i in lower}
+        special = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "{", "}", "[", "]", "|", ";", ":",
+                   "'", '"', ",", "<", ".", ">", "/", "?", "`", "~"}
         password = ""
         pw = []
 
